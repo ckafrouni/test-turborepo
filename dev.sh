5fd749372d66226d68d1b52eeafceba081091b23
Christophe Kafrouni <44137603+ckafrouni@users.noreply.github.com>
2025-05-04 01:45:38 +0200

    feat: add docker compose setup with PostgreSQL and migrate from Vercel to native Postgres client

---
+++ dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker compose -f docker-compose.prod.yml up --build -d
+pnpm db:push
--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,15 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 
 import * as schema from "./schema";
 
+if (!process.env.POSTGRES_URL) {
+  throw new Error("Missing POSTGRES_URL");
+}
+
 export const db = drizzle({
-  client: sql,
   schema,
+  connection: {
+    url: process.env.POSTGRES_URL,
+  },
   casing: "snake_case",
 });
