b4f6d790c7254e1e68abcf6b2ae5ceb3109f999a
Christophe Kafrouni <44137603+ckafrouni@users.noreply.github.com>
2025-05-04 02:54:11 +0200

    feat: add development docker-compose config with PostgreSQL database setup

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker compose -f docker-compose.prod.yml up --build -d
+docker compose -f docker-compose.dev.yml down -v
+docker compose -f docker-compose.dev.yml up --build -d
 pnpm db:push
